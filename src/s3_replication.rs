6ca361008bb2e6a4b8f697286fbff950a213d075
Cameron Elliott <868689+cameronelliott@users.noreply.github.com>
2024-01-26 17:25:29 -0800

    mvp untested, get, put, del, head

--- src/s3_replication.rs
+++ src/s3_replication.rs
@@ -1,12 +1,16 @@
 #![forbid(unsafe_code)]
 #![deny(clippy::all, clippy::pedantic)]
 
+use bytes::Bytes;
 use futures::stream::FuturesUnordered;
 use futures::StreamExt;
 
 use derivative::Derivative;
 
+use s3s::dto::builders::DeleteObjectInputBuilder;
 use s3s::dto::builders::GetObjectInputBuilder;
+
+use s3s::dto::builders::PutObjectInputBuilder;
 use s3s::dto::GetObjectInput;
 use s3s::dto::GetObjectOutput;
 use s3s::dto::*;
@@ -16,6 +20,9 @@ use s3s::S3Result;
 
 use s3s::S3;
 use s3s::{S3Request, S3Response};
+
+use crate::utils::copy_bytes;
+use crate::vec_byte_stream::VecByteStream;
 
 #[derive(Derivative)]
 #[derivative(Debug)]
@@ -76,7 +83,7 @@ impl<T: S3 + std::fmt::Debug + Default> S3 for S3Replication<T> {
         let all_err = results.iter().all(|x| x.is_err());
 
         if all_err {
-            let errs = results
+            let _errs = results
                 .iter()
                 .filter(|x| x.is_err()) // redundant
                 .map(|x| match x {
@@ -85,15 +92,16 @@ impl<T: S3 + std::fmt::Debug + Default> S3 for S3Replication<T> {
                 })
                 .collect::<Vec<_>>();
 
-            if all_elements_equal(&errs) {
-                let o = results.remove(0);
-                return match o {
-                    Ok(_) => panic!(),
-                    Err(e) => Err(e),
-                };
-            }
+            // if all_elements_equal(&errs) {
+            //     let o = results.remove(0);
+            //     return match o {
+            //         Ok(_) => panic!(),
+            //         Err(e) => Err(e),
+            //     };
+            // }
+            return results.remove(0);
         } else if all_ok {
-            let ok = results
+            let _ok = results
                 .iter()
                 .filter(|x| x.is_ok()) // redundant
                 .map(|x| match x {
@@ -107,9 +115,10 @@ impl<T: S3 + std::fmt::Debug + Default> S3 for S3Replication<T> {
                 })
                 .collect::<Vec<_>>();
 
-            if all_elements_equal(&ok) {
-                return results.remove(0);
-            }
+            // if all_elements_equal(&ok) {
+            //     return results.remove(0);
+            // }
+            return results.remove(0);
         } else if !all_ok && !all_err {
             //mixed results
 
@@ -140,16 +149,140 @@ impl<T: S3 + std::fmt::Debug + Default> S3 for S3Replication<T> {
 
     async fn put_object(
         self: &S3Replication<T>,
-        _req: S3Request<PutObjectInput>,
+        req: S3Request<PutObjectInput>,
     ) -> S3Result<S3Response<PutObjectOutput>> {
+        //
+        let PutObjectInput {
+            key, bucket, body, ..
+        } = req.input;
+
+        let stream = body.unwrap();
+        let mut result = Vec::new();
+        copy_bytes(stream, &mut result).await?;
+
+        let bytes = Bytes::from(result);
+
+        let fo = FuturesUnordered::new();
+        for x in self.tgts.iter() {
+            let streaming_blob = StreamingBlob::new(VecByteStream::new(vec![bytes.clone()]));
+            let b = PutObjectInputBuilder::default()
+                .bucket(bucket.clone())
+                .key(key.clone())
+                .body(Some(streaming_blob.into()))
+                .build()
+                .unwrap();
+
+            let f1 = x.put_object(S3Request::new(b));
+
+            fo.push(f1);
+        }
+
+        let mut results = fo.collect::<Vec<_>>().await;
+
+        let all_ok = results.iter().all(|x| x.is_ok());
+        let all_err = results.iter().all(|x| x.is_err());
+
+        if all_err {
+            let _errs = results
+                .iter()
+                .filter(|x| x.is_err()) // redundant
+                .map(|x| match x {
+                    Ok(_) => "".to_string(),
+                    Err(e) => format!("{:?}", e),
+                })
+                .collect::<Vec<_>>();
+
+            // if all_elements_equal(&errs) {
+            //     let o = results.remove(0);
+            //     return match o {
+            //         Ok(_) => panic!(),
+            //         Err(e) => Err(e),
+            //     };
+            // }
+            return results.remove(0);
+        } else if all_ok {
+            return results.remove(0);
+        } else if !all_ok && !all_err {
+            //mixed results
+
+            let first_ok_index = results.iter().position(|x| x.is_ok()).unwrap();
+
+            return results.remove(first_ok_index);
+        }
+
         Err(s3_error!(NoSuchKey))
     }
 
     async fn delete_object(
         &self,
-        _req: S3Request<DeleteObjectInput>,
+        req: S3Request<DeleteObjectInput>,
     ) -> S3Result<S3Response<DeleteObjectOutput>> {
-        Err(s3_error!(NoSuchKey))
+        //
+        let DeleteObjectInput { bucket, key, .. } = req.input;
+
+        let fo = FuturesUnordered::new();
+        for x in self.tgts.iter() {
+            let b = DeleteObjectInputBuilder::default()
+                .bucket(bucket.clone())
+                .key(key.clone())
+                .build()
+                .unwrap();
+
+            let f1 = x.delete_object(S3Request::new(b));
+
+            fo.push(f1);
+        }
+
+        let mut results = fo.collect::<Vec<_>>().await;
+
+        let all_ok = results.iter().all(|x| x.is_ok());
+        let all_err = results.iter().all(|x| x.is_err());
+
+        if all_err {
+            let _errs = results
+                .iter()
+                .filter(|x| x.is_err()) // redundant
+                .map(|x| match x {
+                    Ok(_) => "".to_string(),
+                    Err(e) => format!("{:?}", e),
+                })
+                .collect::<Vec<_>>();
+
+            // if all_elements_equal(&errs) {
+            //     let o = results.remove(0);
+            //     return match o {
+            //         Ok(_) => panic!(),
+            //         Err(e) => Err(e),
+            //     };
+            // }
+            return results.remove(0);
+        } else if all_ok {
+            let _ok = results
+                .iter()
+                .filter(|x| x.is_ok()) // redundant
+                .map(|x| match x {
+                    Ok(kk) => (
+                        kk.output.delete_marker,
+                        //    kk.output.content_type,
+                        //  kk.output.last_modified,
+                        //  kk.output.metadata.clone(),// XX slow!
+                    ),
+                    Err(_e) => panic!(),
+                })
+                .collect::<Vec<_>>();
+
+            // if all_elements_equal(&ok) {
+            //     return results.remove(0);
+            // }
+            return results.remove(0);
+        } else if !all_ok && !all_err {
+            //mixed results
+
+            let first_ok_index = results.iter().position(|x| x.is_ok()).unwrap();
+
+            return results.remove(first_ok_index);
+        }
+        panic!("should not get here");
     }
 
     #[tracing::instrument(skip_all)]
@@ -189,7 +322,7 @@ impl<T: S3 + std::fmt::Debug + Default> S3 for S3Replication<T> {
     }
 }
 
-fn all_elements_equal<T: PartialEq>(vec: &[T]) -> bool {
+fn _all_elements_equal<T: PartialEq>(vec: &[T]) -> bool {
     if vec.is_empty() {
         return true; // or false, depending on your definition for empty vectors
     }
@@ -202,3 +335,19 @@ fn all_elements_equal<T: PartialEq>(vec: &[T]) -> bool {
 pub fn hex(input: impl AsRef<[u8]>) -> String {
     hex_simd::encode_to_string(input.as_ref(), hex_simd::AsciiCase::Lower)
 }
+
+// async fn read_stream(
+//     stream: impl Stream<Item = Result<bytes::Bytes, Box<dyn std::error::Error>>>,
+// ) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+//     let stream = stream;
+//     let mut result = Vec::new();
+
+//     pin_mut!(stream); // needed for iteration
+
+//     while let Some(item) = stream.next().await {
+//         let bytes = item?;
+//         result.extend(bytes);
+//     }
+
+//     Ok(result)
+// }
