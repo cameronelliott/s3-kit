b021dd121a54532c9971245ed16b0516bfa3420c
Cameron Elliott <868689+cameronelliott@users.noreply.github.com>
2024-01-31 22:40:08 -0800

    reducing uncovered code

--- src/s3_replication.rs
+++ src/s3_replication.rs
@@ -117,32 +117,13 @@ impl<T: S3 + std::fmt::Debug + Default> S3 for S3Replication<T> {
             //     return results.remove(0);
             // }
             return results.remove(0);
-        } else if !all_ok && !all_err {
-            //mixed results
-
-            let first_ok_index = results.iter().position(|x| x.is_ok()).unwrap();
-
-            return results.remove(first_ok_index);
         }
-        panic!("should not get here");
+        //else if !all_ok && !all_err {
+        //mixed results
 
-        // while let Some(result) = fo.next().await {
-        //     // Handle result
-        //     info!("Got result: is_ok: {}", result.is_ok());
-        // }
+        let first_ok_index = results.iter().position(|x| x.is_ok()).unwrap();
 
-        //XXXX
-        // // TODO: detect content type
-        // let content_type = mime::APPLICATION_OCTET_STREAM;
-
-        // let output = HeadObjectOutput {
-        //     content_length: try_!(i64::try_from(len)),
-        //     content_type: Some(content_type),
-        //     last_modified: None,
-        //     metadata: None,
-        //     ..Default::default()
-        // };
-        // Ok(S3Response::new(output))
+        return results.remove(first_ok_index);
     }
 
     #[tracing::instrument(skip_all)]
@@ -186,6 +167,17 @@ impl<T: S3 + std::fmt::Debug + Default> S3 for S3Replication<T> {
         let all_ok = results.iter().all(|x| x.is_ok());
         let all_err = results.iter().all(|x| x.is_err());
 
+        let num_ok = results.iter().filter(|x| x.is_ok()).count();
+        let num_err = results.iter().filter(|x| x.is_err()).count();
+        println!(
+            "nresults {} numok {} numerr {} all_ok {} all_err {}",
+            results.len(),
+            num_ok,
+            num_err,
+            all_ok,
+            all_err
+        );
+
         if all_err {
             // let _errs = results
             //     .iter()
@@ -203,18 +195,17 @@ impl<T: S3 + std::fmt::Debug + Default> S3 for S3Replication<T> {
             //         Err(e) => Err(e),
             //     };
             // }
+
             return results.remove(0);
         } else if all_ok {
             return results.remove(0);
-        } else if !all_ok && !all_err {
-            //mixed results
-
-            let first_ok_index = results.iter().position(|x| x.is_ok()).unwrap();
-
-            return results.remove(first_ok_index);
         }
+        // if !all_ok && !all_err {
+        //mixed results
 
-        Err(s3_error!(NoSuchKey))
+        let ok_ix = results.iter().position(|x| x.is_ok()).unwrap();
+
+        return results.remove(ok_ix);
     }
 
     #[tracing::instrument(skip_all)]
@@ -280,14 +271,13 @@ impl<T: S3 + std::fmt::Debug + Default> S3 for S3Replication<T> {
             //     return results.remove(0);
             // }
             return results.remove(0);
-        } else if !all_ok && !all_err {
-            //mixed results
-
-            let first_ok_index = results.iter().position(|x| x.is_ok()).unwrap();
-
-            return results.remove(first_ok_index);
         }
-        panic!("should not get here");
+        //else if !all_ok && !all_err {
+        //mixed results
+
+        let first_ok_index = results.iter().position(|x| x.is_ok()).unwrap();
+
+        return results.remove(first_ok_index);
     }
 
     #[tracing::instrument(skip_all)]
@@ -326,33 +316,3 @@ impl<T: S3 + std::fmt::Debug + Default> S3 for S3Replication<T> {
         Ok(S3Response::new(output))
     }
 }
-
-// fn _all_elements_equal<T: PartialEq>(vec: &[T]) -> bool {
-//     if vec.is_empty() {
-//         return true; // or false, depending on your definition for empty vectors
-//     }
-
-//     let first = &vec[0];
-//     vec.iter().all(|item| item == first)
-// }
-
-// from s3s, s3s copyright stands
-pub fn hex(input: impl AsRef<[u8]>) -> String {
-    hex_simd::encode_to_string(input.as_ref(), hex_simd::AsciiCase::Lower)
-}
-
-// async fn read_stream(
-//     stream: impl Stream<Item = Result<bytes::Bytes, Box<dyn std::error::Error>>>,
-// ) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
-//     let stream = stream;
-//     let mut result = Vec::new();
-
-//     pin_mut!(stream); // needed for iteration
-
-//     while let Some(item) = stream.next().await {
-//         let bytes = item?;
-//         result.extend(bytes);
-//     }
-
-//     Ok(result)
-// }
